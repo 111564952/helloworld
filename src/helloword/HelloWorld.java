14cfca64270ccb3b8b92c2d5219c029bd989ae26
11156 <11156@DESKTOP-RNNFHAE>
2019-05-07 16:48:30 +0800

    wang

--- src/helloword/HelloWorld.java
+++ src/helloword/HelloWorld.java
@@ -2,6 +2,6 @@ package helloword;
 
 public class HelloWorld {
 	  public static void main(String[] args) {
-	        System.out.println("Hello World wang sascac");
+	        System.out.println("Hello World wang aaaaa");
 	    }
 }
